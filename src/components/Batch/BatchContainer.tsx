d3098481cb26c37639f463df959f5e51b6095340
RamsPantoja <worddraco1@gmail.com>
2024-03-27 19:33:33 -0600

    Testing: Discord webhook for this repo

--- src/components/Batch/BatchContainer.tsx
+++ src/components/Batch/BatchContainer.tsx
@@ -5,6 +5,7 @@ const BatchContainer = () => {
         <div className="flex flex-col gap-4 w-full">
             <p className=" text-whiteMain text-4xl font-black">Rams tanda</p>
             <p className=" text-whiteMain text-base font-black">Recuerda que una tanda se basa en la confianza y esfuerzo de los participantes.</p>
+            <p>Hola mundo!</p>
         </div>
     )
 }
