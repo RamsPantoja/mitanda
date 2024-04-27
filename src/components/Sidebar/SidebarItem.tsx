b320cc467385ed5c24361463b2abad0550bdbc67
RamsPantoja <worddraco1@gmail.com>
2024-04-26 20:48:22 -0600

    Fix: Activate the menu item if the path includes the base path

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { SidebarItemProps } from "./_types";
+import { type SidebarItemProps } from "./_types";
 
 
 const SibedarItem = ({ href, Icon, label }: SidebarItemProps) => {
@@ -12,21 +12,22 @@ const SibedarItem = ({ href, Icon, label }: SidebarItemProps) => {
     return (
         <li className="flex flex-row gap-2 items-center">
             <Link className="flex w-full" href={href}>
-                <div className={cn({
-                    "flex flex-row gap-2 items-center p-2 w-full rounded-md": pathname !== href,
-                    "flex flex-row gap-2 items-center p-2 w-full rounded-md bg-blackMain": pathname === href
-                })}>
-                    <Icon className={cn({
-                        "w-4 h-4": true,
-                        "text-grayMain": pathname !== href,
-                        " text-whiteMain": pathname === href
-                    })} />
+                <div className={cn(
+                    "flex flex-row gap-2 items-center p-2 w-full rounded-md",
+                    {
+                        "bg-blackMain": pathname.includes(href)
+                    })}>
+                    <Icon className={cn(
+                        "w-4 h-4 text-grayMain",
+                        {
+                            " text-whiteMain": pathname.includes(href)
+                        })} />
                     <p
-                        className={cn({
-                            "text-sm font-bold": true,
-                            "text-grayMain": pathname !== href,
-                            " text-whiteMain": pathname === href
-                        })}
+                        className={cn(
+                            "text-sm font-bold text-grayMain",
+                            {
+                                " text-whiteMain": pathname.includes(href)
+                            })}
                     >{label}</p>
                 </div>
             </Link>
